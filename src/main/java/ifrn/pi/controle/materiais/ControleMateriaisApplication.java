0858138cf1fc4af71a1422cbcfffb9188757e3d6
Luiz Manoel <manoeljusti9@gmail.com>
2022-12-10 14:45:50 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/main/java/ifrn/pi/controle/materiais/ControleMateriaisApplication.java
@@ -0,0 +1,13 @@
+package ifrn.pi.controle.materiais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleMateriaisApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ControleMateriaisApplication.class, args);
+	}
+
+}

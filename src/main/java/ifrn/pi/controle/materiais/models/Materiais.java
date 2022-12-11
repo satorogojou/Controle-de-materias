f28c59d225cf7efdaefa4f24e4eb5460253a3a68
Luiz Manoel <manoeljusti9@gmail.com>
2022-12-11 16:51:58 -0300

    Configurando a classe modelo dos materiais

--- src/main/java/ifrn/pi/controle/materiais/models/Materiais.java
+++ src/main/java/ifrn/pi/controle/materiais/models/Materiais.java
@@ -3,20 +3,25 @@ package ifrn.pi.controle.materiais.models;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Materiais {
 	
-	@id
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	
 	private String nome;
-	private String quantidade;
+	private String material;
+	private Number quantidade;
 	@DateTimeFormat(pattern = "yyyy-MM-dds")
 	private LocalDate data;
 	private LocalTime horário;
@@ -33,10 +38,10 @@ public class Materiais {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getQuantidade() {
+	public Number getQuantidade() {
 		return quantidade;
 	}
-	public void setQuantidade(String quantidade) {
+	public void setQuantidade(Number quantidade) {
 		this.quantidade = quantidade;
 	}
 	public LocalDate getData() {
@@ -55,7 +60,13 @@ public class Materiais {
 	@Override
 	public String toString() {
 		return "Materiais [id=" + id + ", nome=" + nome + ", quantidade=" + quantidade + ", data=" + data + ", horário="
-				+ horário + "]";
+				+ horário + ", material=" + material + "]";
+	}
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
 	}
 	
 	
